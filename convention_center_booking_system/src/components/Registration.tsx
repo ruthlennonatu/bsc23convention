38540ca473ca61c8222e0c833c7595a30dde9ebf
John Devaney <johndevaney1@hotmail.com>
2023-11-09 17:13:21 +0000

    Placed registration labels and inputs into a table for neater look

--- convention_center_booking_system/src/components/Registration.tsx
+++ convention_center_booking_system/src/components/Registration.tsx
@@ -1,75 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
-
 function Registration() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-    <Container>
-      <Row className="vh-100 d-flex justify-content-center align-items-center">
-        <Col md={8} lg={6} xs={12}>
-          <Card className="px-4">
-            <Card.Body>
-              <div className="mb-3 mt-md-4">
-                <h2 className="fw-bold mb-2 text-center text-uppercase ">
-                  Logo
-                </h2>
-                <div className="mb-3">
-                  <Form>
-                    <Form.Group className="mb-3" controlId="Name">
-                      <Form.Label className="text-center">Name</Form.Label>
-                      <Form.Control type="text" placeholder="Enter Name" />
-                    </Form.Group>
+    <form>
+      <h3>Register</h3>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Label className="text-center">
-                        Email address
-                      </Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
+      <table>
+        <tr>
+          <td><label>Email address: </label></td>
+          <td><input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          /></td>
+        </tr>
+        <tr>
+          <td><label>Repeat Email address: </label></td>
+          <td><input
+            type="email"
+            className="form-control"
+            placeholder="Repeat email"
+          /></td>
+        </tr>
+        <tr>
+          <td><label>Password: </label></td>
+          <td><input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          /></td>
+        </tr>
+        <tr>
+          <td><label>Repeat Password: </label></td>
+          <td><input
+            type="password"
+            className="form-control"
+            placeholder="Repeat password"
+          /></td>
+        </tr>
+      </table>
 
-                    <Form.Group
-                      className="mb-3"
-                      controlId="formBasicPassword"
-                    >
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="formBasicPassword"
-                    >
-                      <Form.Label>Confirm Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="formBasicCheckbox"
-                    ></Form.Group>
-                    <div className="d-grid">
-                      <Button variant="primary" type="submit">
-                        Create Account
-                      </Button>
-                    </div>
-                  </Form>
-                  <div className="mt-3">
-                    <p className="mb-0  text-center">
-                      Already have an account??{' '}
-                      <a href="{''}" className="text-primary fw-bold">
-                        Sign In
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="customCheck1"
+          />
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Remember me
+          </label>
+        </div>
+      </div>
+      <div className="d-grid">
+        <br />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+    </form>
   )
 }
 
