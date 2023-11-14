56a02f236f04306da0236245a9069e951ed0e6a3
Conor2000 <l00141197@atu.ie>
2023-11-14 10:23:44 +0000

    added navbar and styles

--- convention_center_booking_system/src/App.tsx
+++ convention_center_booking_system/src/App.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import RegisterComponent from './components/registration'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import RegisterComponent from "./components/Registration";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Nav/Navbar";
 
 function App() {
-
   return (
     <>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/register" element={<RegisterComponent />} />
+          </Routes>
+        </div>
+      </Router>
       <RegisterComponent />
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
