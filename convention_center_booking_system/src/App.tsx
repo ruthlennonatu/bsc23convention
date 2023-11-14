5d654c957c5150b9958115ef88f28a49b581fc94
Aaron Darcy <darcy.aaron@hotmail.com>
2023-11-14 10:57:08 +0000

    Added Login component

--- convention_center_booking_system/src/App.tsx
+++ convention_center_booking_system/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import RegisterComponent from "./components/Registration";
+import LoginComponent from './components/Login';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Nav/Navbar";
 
@@ -12,11 +13,12 @@ function App() {
         <div>
           <Navbar />
           <Routes>
+            <Route path="/" element={<LoginComponent />} />
             <Route path="/register" element={<RegisterComponent />} />
           </Routes>
         </div>
       </Router>
-      <RegisterComponent />
+
     </>
   );
 }
--- convention_center_booking_system/src/components/Login.js
+++
@@ -1,8 +0,0 @@
-//Login Component 
-import React from 'react'
-
-export const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
