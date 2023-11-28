e02630281a1bddbcb41ec486f07d8e85e53ad235
John Devaney <johndevaney1@hotmail.com>
2023-11-28 18:40:57 +0000

    Changed a lot of setting to get jest working

--- convention_center_booking_system/src/components/JestTests/ComponetRendering.test.js
+++ convention_center_booking_system/src/components/JestTests/ComponetRendering.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import Footer from '../Footer';
 import { render } from '@testing-library/react';
-import Footer from './components/Footer';
 
 test('renders Footer without crashing', () => {
   render(<Footer />);
