399c5451e82da80d930a0ac3284952afefd7e16a
Conor2000 <l00141197@atu.ie>
2023-12-05 19:55:50 +0000

    added comments to login unit test code

--- convention_center_booking_system/src/components/JestTests/LoginUnitTest.test.js
+++ convention_center_booking_system/src/components/JestTests/LoginUnitTest.test.js
@@ -17,6 +17,7 @@ import LoginComponent  from '../Login';
 
 describe('LoginComponent', () => {
   it('renders correctly', () => {
+    // Render the LoginComponent within a Router component
     render(
       <Router>
         <LoginComponent />
@@ -29,47 +30,65 @@ describe('LoginComponent', () => {
   });
 
   it('updates email and password fields on user input', () => {
+    // Render the LoginComponent within a Router component
     render(
       <Router>
         <LoginComponent />
       </Router>
     );
 
+    // Get email input
     const emailInput = screen.getByPlaceholderText('Enter email');
+    // Update input value if changed
     fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    // Test inputted email to match correct format
     expect(emailInput.value).toBe('test@example.com');
-
+    
+    // Get password input
     const passwordInput = screen.getByPlaceholderText('Enter password');
+    // Update input value if changed
     fireEvent.change(passwordInput, { target: { value: 'password123' } });
+    // Compare inputted password against password123
     expect(passwordInput.value).toBe('password123');
   });
   
 
   it('handles login button click', () => {
+    // Spy on the console.log method to track its usage
     const consoleSpy = jest.spyOn(console, 'log');
+
+    // Render the LoginComponent within a Router component
     render(
       <Router>
         <LoginComponent />
       </Router>
     );
 
+    // Find the login button in the rendered component by role and name
     const loginButton = screen.getByRole('button', { name: 'Login' });
+    // Simulate a click on the login button
     fireEvent.click(loginButton);
 
+    // Check if console.log was called with the expected message when the button was clicked
     expect(consoleSpy).toHaveBeenCalledWith('Login button clicked');
   });
 
   it('handles register redirect button click', () => {
+    // Spy on the console.log method to track its usage
     const consoleSpy = jest.spyOn(console, 'log');
+    // Render the LoginComponent within a Router component
     render(
       <Router>
         <LoginComponent />
       </Router>
     );
 
+    // Find the register button in the rendered component by its text content
     const registerButton = screen.getByText("Don't have an account? Register");
+    // Simulate a click on the register button
     fireEvent.click(registerButton);
 
+    // Check if console.log was called with the expected message when the button was clicked
     expect(consoleSpy).toHaveBeenCalledWith('Redirect to register');
   });
 });
