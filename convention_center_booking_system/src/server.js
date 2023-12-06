3d801285721268ae7c3f0f40c1492137b8d6173c
john-dev <l00161731@atu.ie>
2023-12-06 12:52:05 +0000

    Added rate limiter for server

--- convention_center_booking_system/src/server.js
+++ convention_center_booking_system/src/server.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import mysql from 'mysql2';
-var app = express();
-
 import 'dotenv/config';
 
+var app = express();
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+
 app.use(express.json()); // For parsing application/json
+
+
 
 // Set up MySQL connection
 var con = mysql.createConnection({
